a2df88282cc6fa21005bad6613e59976f94cdff4
JC Lévesque <levesque.jc@gmail.com>
2014-03-23 21:46:48 -0400

    New chooser module which integrates expected improvement, lower confidence bound and plotting abilities. Also got rid of command-line style chooser args since they were a pain and rather useless to me.

--- spearmint/spearmint/chooser/GPChooser.py
+++ spearmint/spearmint/chooser/GPChooser.py
@@ -1,4 +1,8 @@
 ##
+# Copyright (C) 2014 Julien-Charles Levesque
+# Same copyright as below (GNU GPL)
+
+##
 # Copyright (C) 2012 Jasper Snoek, Hugo Larochelle and Ryan P. Adams
 #
 # This code is written for research and educational purposes only to
@@ -35,20 +39,23 @@ from ..helpers import *
 from ..Locker  import *
 
 
-def init(expt_dir, arg_string):
-    args = util.unpack_args(arg_string)
-    return GPEIChooser(expt_dir, **args)
+def init(expt_dir, args):
+    return GPChooser(expt_dir, **args)
 
 """
-Chooser module for the Gaussian process expected improvement
-acquisition function.  Candidates are sampled densely in the unit
-hypercube and then the highest EI point is selected.  Slice sampling
-is used to sample Gaussian process hyperparameters for the GP.
+Chooser module for the Gaussian process acquisition function.
+
+Candidates are sampled densely in the unit
+hypercube and then the highest point according to `acquisition_func` is
+selected.  Slice sampling is used to sample Gaussian process hyperparameters
+for the GP.
 """
-class GPEIChooser:
+class GPChooser:
 
     def __init__(self, expt_dir, covar="Matern52", mcmc_iters=10,
-                 pending_samples=100, noiseless=False):
+            acquisition_func='EI', acquisition_params={},
+            pending_samples=100, noiseless=False,
+            save_acquisition_figures=False):
         self.cov_func        = getattr(gp, covar)
         self.locker          = Locker()
         self.state_pkl       = os.path.join(expt_dir, self.__module__ + ".pkl")
@@ -62,6 +69,18 @@ class GPEIChooser:
         self.noise_scale = 0.1  # horseshoe prior
         self.amp2_scale  = 1    # zero-mean log normal prior
         self.max_ls      = 2    # top-hat prior on length scales
+
+
+        self.acquisition_func = acquisition_func
+        self.acquisition_params = acquisition_params
+        if self.acquisition_func == 'EI':
+            self.compute_acquisition = compute_ei
+            self.sort_func = np.argmax
+        elif self.acquisition_func == 'LCB':
+            self.compute_acquisition = compute_lcb
+            self.sort_func = np.argmin
+
+        self.save_acquisition_figures = save_acquisition_figures
 
     def __del__(self):
         self.locker.lock_wait(self.state_pkl)
@@ -140,54 +159,27 @@ class GPEIChooser:
         if self.mcmc_iters > 0:
             # Sample from hyperparameters.
 
-            overall_ei = np.zeros((cand.shape[0], self.mcmc_iters))
-            gp_mean = np.zeros((cand.shape[0], self.mcmc_iters))
+            overall_acq = np.zeros((cand.shape[0], self.mcmc_iters))
+            m_mean = np.zeros((cand.shape[0], self.mcmc_iters))
+            m_var = np.zeros((cand.shape[0], self.mcmc_iters))
 
             for mcmc_iter in range(self.mcmc_iters):
 
                 self.sample_hypers(comp, vals)
                 log("mean: %f  amp: %f  noise: %f  min_ls: %f  max_ls: %f"
-                                 % (self.mean, np.sqrt(self.amp2), self.noise, np.min(self.ls), np.max(self.ls)))
+                    % (self.mean, np.sqrt(self.amp2), self.noise,
+                    np.min(self.ls), np.max(self.ls)))
 
-                overall_ei[:, mcmc_iter], gp_mean[:, mcmc_iter] = self.compute_ei(comp, pend, cand, vals)
+                overall_acq[:, mcmc_iter], m_mean[:, mcmc_iter], m_var[:, mcmc_iter] = \
+                 self.compute_marginals_and_acquisition(comp, pend, cand, vals)
 
-            mean_ei = np.mean(overall_ei, axis=1)
-            best_cand = np.argmax(mean_ei)
+            acq = np.mean(overall_acq, axis=1)
+            best_cand = self.sort_func(acq)
 
-            std_ei = np.std(overall_ei, axis=1)
-
-            import matplotlib.pyplot as plt
-            fig = plt.figure()
-            ax = fig.add_subplot(211)
-            sort_i = np.argsort(cand[:, 0]) #hack, use only one parameter for alpha value selection debugging.
-            #import ipdb; ipdb.set_trace()
-            gp_mean_mean = np.mean(gp_mean, axis=1)
-            gp_mean_std = np.std(gp_mean, axis=1)
-
-            ax.scatter(cand[:, 0][best_cand], mean_ei[best_cand], c='r', s=30)
-            ax.plot(cand[:, 0][sort_i], mean_ei[sort_i])
-            #ax.plot(cand[:, 0][sort_i], mean_ei[sort_i] + std_ei[sort_i], '--g')
-            #ax.plot(cand[:, 0][sort_i], mean_ei[sort_i] - std_ei[sort_i], '--g')
-
-            for i in range(self.mcmc_iters):
-                ax.plot(cand[:, 0][sort_i], overall_ei[:, i][sort_i], '0.5', alpha=0.5)
-
-            ax.set_xlim((0, 1))
-
-            ax = fig.add_subplot(212)
-            ax.scatter(comp[:, 0], vals)
-            ax.plot(cand[:, 0][sort_i], gp_mean_mean[sort_i])
-            #ax.plot(cand[:, 0][sort_i], gp_mean_mean[sort_i] + gp_mean_std[sort_i], '--g')
-            #ax.plot(cand[:, 0][sort_i], gp_mean_mean[sort_i] - gp_mean_std[sort_i], '--g')
-
-            for i in range(self.mcmc_iters):
-                ax.plot(cand[:, 0][sort_i], gp_mean[:, i][sort_i], '0.5', alpha=0.5)
-
-            ax.set_xlim((0, 1))
-            fig.savefig('EI_%i.png' % (len(comp)))
-
+            if self.save_acquisition_figures:
+                plot_mean_and_acquisition(cand, best_cand, comp, vals, acq,
+                 overall_acq, m_mean, m_var, self.acquisition_func, self.noiseless)
             return int(candidates[best_cand])
-
         else:
             # Optimize hyperparameters
             try:
@@ -203,101 +195,11 @@ class GPEIChooser:
                              % (self.mean, np.sqrt(self.amp2), self.noise, np.min(self.ls),
                                 np.max(self.ls)))
 
-            ei = self.compute_ei(comp, pend, cand, vals)
+            acq, _, _ = self.compute_marginals_and_acquisition(comp, pend, cand, vals)
 
-            best_cand = np.argmax(ei)
+            best_cand = self.sort_func(acq)
 
             return int(candidates[best_cand])
-
-    def compute_ei(self, comp, pend, cand, vals):
-        if pend.shape[0] == 0:
-            # If there are no pending, don't do anything fancy.
-
-            # Current best.
-            best = np.min(vals)
-
-            # The primary covariances for prediction.
-            comp_cov   = self.cov(comp)
-            cand_cross = self.cov(comp, cand)
-
-            # Compute the required Cholesky.
-            obsv_cov  = comp_cov + self.noise*np.eye(comp.shape[0])
-            obsv_chol = spla.cholesky( obsv_cov, lower=True )
-
-            # Solve the linear systems.
-            alpha  = spla.cho_solve((obsv_chol, True), vals - self.mean)
-            beta   = spla.solve_triangular(obsv_chol, cand_cross, lower=True)
-
-            # Predict the marginal means and variances at candidates.
-            func_m = np.dot(cand_cross.T, alpha) + self.mean
-            func_v = self.amp2*(1+1e-6) - np.sum(beta**2, axis=0)
-
-            # Expected improvement
-            func_s = np.sqrt(func_v)
-            u      = (best - func_m) / func_s
-            ncdf   = sps.norm.cdf(u)
-            npdf   = sps.norm.pdf(u)
-            ei     = func_s*( u*ncdf + npdf)
-
-            return ei, func_m
-        else:
-            # If there are pending experiments, fantasize their outcomes.
-
-            # Create a composite vector of complete and pending.
-            comp_pend = np.concatenate((comp, pend))
-
-            # Compute the covariance and Cholesky decomposition.
-            comp_pend_cov  = self.cov(comp_pend) + self.noise*np.eye(comp_pend.shape[0])
-            comp_pend_chol = spla.cholesky(comp_pend_cov, lower=True)
-
-            # Compute submatrices.
-            pend_cross = self.cov(comp, pend)
-            pend_kappa = self.cov(pend)
-
-            # Use the sub-Cholesky.
-            obsv_chol = comp_pend_chol[:comp.shape[0],:comp.shape[0]]
-
-            # Solve the linear systems.
-            alpha  = spla.cho_solve((obsv_chol, True), vals - self.mean)
-            beta   = spla.cho_solve((obsv_chol, True), pend_cross)
-
-            # Finding predictive means and variances.
-            pend_m = np.dot(pend_cross.T, alpha) + self.mean
-            pend_K = pend_kappa - np.dot(pend_cross.T, beta)
-
-            # Take the Cholesky of the predictive covariance.
-            pend_chol = spla.cholesky(pend_K, lower=True)
-
-            # Make predictions.
-            pend_fant = (np.dot(pend_chol, npr.randn(pend.shape[0],self.pending_samples))
-                         + pend_m[:,None])
-
-            # Include the fantasies.
-            fant_vals = np.concatenate((np.tile(vals[:,np.newaxis],
-                                                (1,self.pending_samples)), pend_fant))
-
-            # Compute bests over the fantasies.
-            bests = np.min(fant_vals, axis=0)
-
-            # Now generalize from these fantasies.
-            cand_cross = self.cov(comp_pend, cand)
-
-            # Solve the linear systems.
-            alpha  = spla.cho_solve((comp_pend_chol, True), fant_vals - self.mean)
-            beta   = spla.solve_triangular(comp_pend_chol, cand_cross, lower=True)
-
-            # Predict the marginal means and variances at candidates.
-            func_m = np.dot(cand_cross.T, alpha) + self.mean
-            func_v = self.amp2*(1+1e-6) - np.sum(beta**2, axis=0)
-
-            # Expected improvement
-            func_s = np.sqrt(func_v[:,np.newaxis])
-            u      = (bests[np.newaxis,:] - func_m) / func_s
-            ncdf   = sps.norm.cdf(u)
-            npdf   = sps.norm.pdf(u)
-            ei     = func_s*( u*ncdf + npdf)
-
-            return np.mean(ei, axis=1), np.mean(func_m, axis=1)
 
     def sample_hypers(self, comp, vals):
         if self.noiseless:
@@ -394,3 +296,146 @@ class GPEIChooser:
 
         return
 
+    def compute_marginals_and_acquisition(self, comp, pend, cand, vals):
+        if pend.shape[0] == 0:
+            # If there are no pending, don't do anything fancy.
+
+            # Current best.
+            best = np.min(vals)
+
+            # The primary covariances for prediction.
+            comp_cov   = self.cov(comp)
+            cand_cross = self.cov(comp, cand)
+
+            # Compute the required Cholesky.
+            obsv_cov  = comp_cov + self.noise*np.eye(comp.shape[0])
+            obsv_chol = spla.cholesky( obsv_cov, lower=True )
+
+            # Solve the linear systems.
+            alpha  = spla.cho_solve((obsv_chol, True), vals - self.mean)
+            beta   = spla.solve_triangular(obsv_chol, cand_cross, lower=True)
+
+            # Predict the marginal means and variances at candidates.
+            func_m = np.dot(cand_cross.T, alpha) + self.mean
+            func_v = self.amp2*(1+1e-6) - np.sum(beta**2, axis=0)
+
+            # Expected improvement
+            func_s = np.sqrt(func_v)
+            acq = self.compute_acquisition(func_m, func_s, best,
+                **self.acquisition_params)
+
+            return acq, func_m, func_s
+        else:
+            # If there are pending experiments, fantasize their outcomes.
+
+            # Create a composite vector of complete and pending.
+            comp_pend = np.concatenate((comp, pend))
+
+            # Compute the covariance and Cholesky decomposition.
+            comp_pend_cov  = self.cov(comp_pend) + self.noise*np.eye(comp_pend.shape[0])
+            comp_pend_chol = spla.cholesky(comp_pend_cov, lower=True)
+
+            # Compute submatrices.
+            pend_cross = self.cov(comp, pend)
+            pend_kappa = self.cov(pend)
+
+            # Use the sub-Cholesky.
+            obsv_chol = comp_pend_chol[:comp.shape[0],:comp.shape[0]]
+
+            # Solve the linear systems.
+            alpha  = spla.cho_solve((obsv_chol, True), vals - self.mean)
+            beta   = spla.cho_solve((obsv_chol, True), pend_cross)
+
+            # Finding predictive means and variances.
+            pend_m = np.dot(pend_cross.T, alpha) + self.mean
+            pend_K = pend_kappa - np.dot(pend_cross.T, beta)
+
+            # Take the Cholesky of the predictive covariance.
+            pend_chol = spla.cholesky(pend_K, lower=True)
+
+            # Make predictions.
+            pend_fant = (np.dot(pend_chol, npr.randn(pend.shape[0],self.pending_samples))
+                         + pend_m[:,None])
+
+            # Include the fantasies.
+            fant_vals = np.concatenate((np.tile(vals[:,np.newaxis],
+                                                (1,self.pending_samples)), pend_fant))
+
+            # Compute bests over the fantasies.
+            bests = np.min(fant_vals, axis=0)
+
+            # Now generalize from these fantasies.
+            cand_cross = self.cov(comp_pend, cand)
+
+            # Solve the linear systems.
+            alpha  = spla.cho_solve((comp_pend_chol, True), fant_vals - self.mean)
+            beta   = spla.solve_triangular(comp_pend_chol, cand_cross, lower=True)
+
+            # Predict the marginal means and variances at candidates.
+            func_m = np.dot(cand_cross.T, alpha) + self.mean
+            func_v = self.amp2*(1+1e-6) - np.sum(beta**2, axis=0)
+
+            # Expected improvement
+            func_s = np.sqrt(func_v[:,np.newaxis])
+
+            acq = self.compute_acquisition(func_m, func_s, bests,
+             **self.acquisition_params)
+
+            #Return average over fantasies
+            return np.mean(acq, axis=1), np.mean(func_m, axis=1), np.mean(func_v, axis=1)
+
+
+def compute_ei(func_m, func_s, bests):
+    try:
+        u = (bests[np.newaxis,:] - func_m) / func_s
+    except:
+        u = (best - func_m) / func_s
+    ncdf = sps.norm.cdf(u)
+    npdf = sps.norm.pdf(u)
+    ei = func_s*( u*ncdf + npdf)
+    return ei
+
+
+def compute_lcb(func_m, func_s, bests, beta):
+    return func_m - beta*func_s
+
+
+def plot_mean_and_acquisition(cand, best_cand, comp, vals, acq, overall_acq,
+ m_mean, m_var, prefix, noiseless):
+    n_cand, mcmc_iters = np.shape(overall_acq)
+
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(211)
+
+    #Won't work for anything other than 1D problems
+    sort_i = np.argsort(cand[:, 0])
+
+    ax.scatter(cand[:, 0][best_cand], acq[best_cand], c='r', s=30)
+    ax.plot(cand[:, 0][sort_i], acq[sort_i])
+    #ax.plot(cand[:, 0][sort_i], mean_lcb[sort_i] + std_ei[sort_i], '--g')
+    #ax.plot(cand[:, 0][sort_i], mean_lcb[sort_i] - std_ei[sort_i], '--g')
+    for i in range(mcmc_iters):
+        ax.plot(cand[:, 0][sort_i], overall_acq[:, i][sort_i], '0.5', alpha=0.5)
+
+    ax.set_xlim((0, 1))
+    ax.set_ylabel(prefix)
+
+    ax = fig.add_subplot(212)
+    ax.scatter(comp[:, 0], vals)
+
+    #Marginal mean mean, I know
+    mmm = np.mean(m_mean, axis=1)
+    mms = np.sqrt(np.mean(m_var, axis=1))
+    ax.plot(cand[:, 0][sort_i], mmm[sort_i])
+    ax.plot(cand[:, 0][sort_i], mmm[sort_i] + mms[sort_i], '--g')
+    ax.plot(cand[:, 0][sort_i], mmm[sort_i] - mms[sort_i], '--g')
+
+    for i in range(mcmc_iters):
+        ax.plot(cand[:, 0][sort_i], m_mean[:, i][sort_i], '0.5', alpha=0.5)
+
+    ax.set_xlim((0, 1))
+    ax.set_ylabel('marginal mean')
+    figname = prefix + ('_noiseless_' if noiseless else 'noisy_') + '%i.png' % (len(comp))
+    fig.tight_layout()
+    fig.savefig(figname)
